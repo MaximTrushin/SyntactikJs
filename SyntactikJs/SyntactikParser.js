c2b16ce10d1eb388f761132f502565c3384405ba
Maxim Trushin <trushin@gmail.com>
2017-03-22 21:03:17 -0400

    Initial commit for unit testing.

--- SyntactikJs/SyntactikParser.js
+++
@@ -1,5 +0,0 @@
-﻿define(function (require, exports, module) {
-"use strict";
-
-
-});
---
+++ SyntactikJs/tests/parser_test.js
@@ -0,0 +1,38 @@
+﻿require("amd-loader");
+
+var fs = require("fs");
+var path = require("path");
+var cwd = __dirname + "/";
+
+var InputStream = require("../InputStream").InputStream;
+var DomPrinter = require("../DomPrinter").DomPrinter;
+var Parser = require("../Parser").Parser;
+
+
+
+
+function test() {
+    var tests = fs.readdirSync(cwd + "/scenarios/").filter(function (fileName) { return fileName.match(/(.*).smx/) });
+    tests.forEach(function (item) { testScenario(item.match(/(.*).smx/)[1])});
+
+};
+
+function testScenario(name) {
+    console.log("Testing scenario " + name + "\n");
+    
+    var code = fs.readFileSync(cwd + "/scenarios/" + name + ".smx", "utf8");
+    console.log("Code:");
+    console.log(code);
+    var input = new InputStream(code); 
+    var parser = new Parser(input);
+    var domPrinter = new DomPrinter();
+    var pair = parser.parseModule();
+    console.log("DOM:");
+    console.log(domPrinter.printModule(pair));
+};
+
+module.exports = {
+    test: test
+}
+
+test();
